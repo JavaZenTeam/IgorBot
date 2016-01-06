7a3782ab3d0b14eb60188fb7f9adec38fee57392
Dmitriy <Daemon712@yandex.ru>
2016-01-06 17:09:54 +0300

    Расширил сущность chat

--- src/main/java/ru/javazen/telegram/bot/entity/Chat.java
+++ src/main/java/ru/javazen/telegram/bot/entity/Chat.java
@@ -12,6 +12,18 @@ public class Chat {
     @JsonProperty("type")
     private String type;
 
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
     public long getId() {
         return id;
     }
@@ -26,5 +38,37 @@ public class Chat {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
