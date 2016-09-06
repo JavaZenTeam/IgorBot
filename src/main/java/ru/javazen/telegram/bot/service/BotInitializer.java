5b45d02eb8716cd148a3caab5292624a27c10fbd
Dmitriy <Daemon712@yandex.ru>
2016-09-06 23:55:31 +0300

    awesome useless refactoring

--- src/main/java/ru/javazen/telegram/bot/service/BotInitializer.java
+++ src/main/java/ru/javazen/telegram/bot/service/BotInitializer.java
@@ -2,23 +2,31 @@ package ru.javazen.telegram.bot.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.javazen.telegram.bot.Bot;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
 import java.util.Map;
 
 @Service
 public class BotInitializer {
     private static final Logger LOGGER = LoggerFactory.getLogger(BotInitializer.class);
+    private Map<String, Bot> bots;
+    private String path;
 
-    @Value("${callbackUrl}")
-    private String callbackUrl;
+    @Autowired
+    public BotInitializer(ServletContext servletContext, @Value("${server.url}")String serverUrl) {
+        path = serverUrl + servletContext.getContextPath() + CallbackService.CALLBACK_PATH + "/";
+    }
 
     @Resource(name = "botMap")
-    private Map<String, Bot> bots;
+    public void setBots(Map<String, Bot> bots) {
+        this.bots = bots;
+    }
 
     @PostConstruct
     public void initBots(){
@@ -29,7 +37,7 @@ public class BotInitializer {
     }
 
     private void initBot(String name, Bot bot){
-        bot.getService().setWebHook(callbackUrl + name);
+        bot.getService().setWebHook(path + name);
         bot.onStart();
     }
 }
