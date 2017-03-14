f7d4f114bc9bdbacd06a6f6ec27984bc69d6157f
Andrew <asmal95@mail.ru>
2017-03-14 22:30:20 +0400

    Added some send sticker handlers

---
+++ src/main/java/ru/javazen/telegram/bot/filter/AllowedChatFilter.java
@@ -0,0 +1,20 @@
+package ru.javazen.telegram.bot.filter;
+
+import ru.javazen.telegram.bot.entity.request.Update;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllowedChatFilter implements Filter {
+
+    private List<Long> allowedChatIds = new ArrayList<>();
+
+    @Override
+    public boolean check(Update update) {
+        return allowedChatIds.contains(update.getMessage().getChat().getId());
+    }
+
+    public void setAllowedChatIds(List<Long> allowedChatIds) {
+        this.allowedChatIds = allowedChatIds;
+    }
+}
