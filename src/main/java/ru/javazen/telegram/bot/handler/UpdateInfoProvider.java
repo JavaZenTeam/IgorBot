aaa30cd7fb9fcba3d6565f47d12e5e8e2ba66284
Dmitriy <Daemon712@yandex.ru>
2017-11-26 23:51:05 +0400

    add feature fro UpdateInfoProvider to specify path of requested entity

--- src/main/java/ru/javazen/telegram/bot/handler/UpdateInfoProvider.java
+++ src/main/java/ru/javazen/telegram/bot/handler/UpdateInfoProvider.java
@@ -1,31 +1,67 @@
 package ru.javazen.telegram.bot.handler;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javazen.telegram.bot.BotMethodExecutor;
 import ru.javazen.telegram.bot.entity.Update;
 import ru.javazen.telegram.bot.method.send.SendMessage;
+import ru.javazen.telegram.bot.util.MessageHelper;
 
+import java.beans.PropertyDescriptor;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 public class UpdateInfoProvider implements UpdateHandler {
 
-    @Autowired
     private ObjectMapper mapper;
+
+    private String invalidPathMessage = "invalid path";
 
     @Override
     public boolean handle(Update update, BotMethodExecutor executor) {
         try {
-            SendMessage message = new SendMessage();
-            message.setChatId(update.getMessage().getChat().getId().toString());
-            String value = mapper.writeValueAsString(update);
-            message.setText("```" + value + "```");
+            String text = MessageHelper.getActualText(update.getMessage());
+            Object requestedEntity = text.split(" ").length > 1
+                    ? resolveEntity(update, text.split(" ")[1])
+                    : update;
+
+            String answer = mapper.writeValueAsString(requestedEntity);
+            SendMessage message = MessageHelper.answer(update.getMessage(), "```" + answer + "```");
             message.setParseMode("MARKDOWN");
             executor.execute(message, Void.class);
             return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
+        } catch (IllegalArgumentException e) {
+            SendMessage message = MessageHelper.answer(update.getMessage(), invalidPathMessage);
+            executor.execute(message, Void.class);
+            return true;
+        } catch (IOException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    @Autowired
+    public void setMapper(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public void setInvalidPathMessage(String InvalidPathMessage) {
+        this.invalidPathMessage = InvalidPathMessage;
+    }
+
+    private Object resolveEntity(Object entity, String path) throws InvocationTargetException, IllegalAccessException {
+        int dotIndex = path.indexOf('.');
+        String pathNode = dotIndex == -1
+                ? path
+                : path.substring(0, dotIndex);
+
+        if (entity == null) throw new IllegalArgumentException("can't take property " + pathNode + "from null");
+        PropertyDescriptor property = BeanUtils.getPropertyDescriptor(entity.getClass(), pathNode);
+        if (property == null) throw new IllegalArgumentException("unknown property " + pathNode);
+        Object result = property.getReadMethod().invoke(entity);
+
+        return dotIndex == -1
+                ? result
+                : resolveEntity(result, path.substring(dotIndex + 1));
     }
 }
