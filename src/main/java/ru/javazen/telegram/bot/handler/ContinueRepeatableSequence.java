153c86cdb8af3f7db8c71132a48dc0ef686f6252
Dmitriy <Daemon712@yandex.ru>
2017-12-04 02:23:12 +0400

    add stickers support to continue repeatable sequence

--- src/main/java/ru/javazen/telegram/bot/handler/ContinueRepeatableSequence.java
+++ src/main/java/ru/javazen/telegram/bot/handler/ContinueRepeatableSequence.java
@@ -1,25 +1,53 @@
 package ru.javazen.telegram.bot.handler;
 
 import ru.javazen.telegram.bot.BotMethodExecutor;
+import ru.javazen.telegram.bot.entity.Message;
+import ru.javazen.telegram.bot.entity.Sticker;
 import ru.javazen.telegram.bot.entity.Update;
+import ru.javazen.telegram.bot.method.send.SendSticker;
 import ru.javazen.telegram.bot.util.MessageHelper;
 
+import java.util.Objects;
+
 public class ContinueRepeatableSequence implements UpdateHandler {
-    private volatile Update lastUpdate;
+    private volatile Message lastMessage;
 
     @Override
     public boolean handle(Update update, BotMethodExecutor executor) {
-        String text = update.getMessage().getText();
-        if (lastUpdate != null && text != null){
-            boolean textEquals = text.equals(lastUpdate.getMessage().getText());
-            boolean fromEquals = update.getMessage().getFrom().getId().equals(lastUpdate.getMessage().getFrom().getId());
+        boolean handled = false;
+        Message newMessage = update.getMessage();
+        if (lastMessage != null && !equalAuthor(newMessage)) {
+            if (equalText(newMessage)) {
+                String text = MessageHelper.getActualText(newMessage);
+                executor.execute(MessageHelper.answer(newMessage, text), Void.class);
+                handled = true;
+            }
 
-            if (textEquals && !fromEquals){
-                executor.execute(MessageHelper.answer(update.getMessage(), text), Void.class);
-                return true;
+            if (equalSticker(newMessage)) {
+                SendSticker sendSticker = new SendSticker();
+                sendSticker.setChatId(newMessage.getChat().getId().toString());
+                sendSticker.setSticker(newMessage.getSticker().getFileId());
+                executor.execute(sendSticker, Void.class);
+                handled = true;
             }
         }
-        lastUpdate = update;
-        return false;
+        lastMessage = newMessage;
+        return handled;
+    }
+
+    private boolean equalText(Message newMessage) {
+        String lastText = MessageHelper.getActualText(lastMessage);
+        String newText = MessageHelper.getActualText(newMessage);
+        return lastText != null && lastText.equals(newText);
+    }
+
+    private boolean equalSticker(Message newMessage) {
+        Sticker lastSticker = lastMessage.getSticker();
+        Sticker newSticker = newMessage.getSticker();
+        return lastSticker != null && lastSticker.getFileId().equals(newSticker.getFileId());
+    }
+
+    private boolean equalAuthor(Message newMessage){
+        return Objects.equals(lastMessage.getFrom().getId(), newMessage.getFrom().getId());
     }
 }
