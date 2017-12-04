f4ed698f90e14166dee1f2bd3d49829ad47d932b
Dmitriy <Daemon712@yandex.ru>
2017-12-05 01:45:52 +0400

    make continue repeatable sequence module works with long sequences (now the length is randomly)

--- src/main/java/ru/javazen/telegram/bot/handler/ContinueRepeatableSequence.java
+++ src/main/java/ru/javazen/telegram/bot/handler/ContinueRepeatableSequence.java
@@ -2,52 +2,89 @@ package ru.javazen.telegram.bot.handler;
 
 import ru.javazen.telegram.bot.BotMethodExecutor;
 import ru.javazen.telegram.bot.entity.Message;
-import ru.javazen.telegram.bot.entity.Sticker;
 import ru.javazen.telegram.bot.entity.Update;
+import ru.javazen.telegram.bot.entity.User;
+import ru.javazen.telegram.bot.method.send.SendMessage;
 import ru.javazen.telegram.bot.method.send.SendSticker;
 import ru.javazen.telegram.bot.util.MessageHelper;
 
-import java.util.Objects;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class ContinueRepeatableSequence implements UpdateHandler {
-    private volatile Message lastMessage;
+    private static final int BUFFER_SIZE = 5;
+    private Queue<Message> messageQueue = new LinkedList<>();
 
     @Override
     public boolean handle(Update update, BotMethodExecutor executor) {
-        boolean handled = false;
-        Message newMessage = update.getMessage();
-        if (lastMessage != null && !equalAuthor(newMessage)) {
-            if (equalText(newMessage)) {
-                String text = MessageHelper.getActualText(newMessage);
-                executor.execute(MessageHelper.answer(newMessage, text), Void.class);
-                handled = true;
-            }
-
-            if (equalSticker(newMessage)) {
-                SendSticker sendSticker = new SendSticker();
-                sendSticker.setChatId(newMessage.getChat().getId().toString());
-                sendSticker.setSticker(newMessage.getSticker().getFileId());
-                executor.execute(sendSticker, Void.class);
-                handled = true;
-            }
+        Message msg = update.getMessage();
+        messageQueue.add(msg);
+        if (messageQueue.size() > BUFFER_SIZE){
+            messageQueue.remove();
         }
-        lastMessage = newMessage;
-        return handled;
+        int required = generateRequiredLength();
+        if (messageQueue.size() < required) {
+            return false;
+        }
+
+        List<Message> messages = messageQueue.stream()
+                .skip(messageQueue.size() - required)
+                .collect(Collectors.toList());
+
+        if (!allUsersUnique(messages)) {
+            return false;
+        }
+        if (allTextsEquals(messages)){
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(msg.getChat().getId().toString());
+            sendMessage.setText(MessageHelper.getActualText(msg));
+            executor.execute(sendMessage, Void.class);
+            messageQueue.clear();
+            return true;
+        }
+        if (allStickersEquals(messages)){
+            SendSticker sendSticker = new SendSticker();
+            sendSticker.setChatId(msg.getChat().getId().toString());
+            sendSticker.setSticker(msg.getSticker().getFileId());
+            executor.execute(sendSticker, Void.class);
+            messageQueue.clear();
+            return true;
+        }
+        return false;
     }
 
-    private boolean equalText(Message newMessage) {
-        String lastText = MessageHelper.getActualText(lastMessage);
-        String newText = MessageHelper.getActualText(newMessage);
-        return lastText != null && lastText.equals(newText);
+    private boolean allUsersUnique(List<Message> messages){
+        long uniqueUsers = messages.stream()
+                .map(Message::getFrom)
+                .map(User::getId)
+                .distinct()
+                .count();
+        return uniqueUsers == messages.size();
     }
 
-    private boolean equalSticker(Message newMessage) {
-        Sticker lastSticker = lastMessage.getSticker();
-        Sticker newSticker = newMessage.getSticker();
-        return lastSticker != null && lastSticker.getFileId().equals(newSticker.getFileId());
+    private boolean allTextsEquals(List<Message> messages){
+        List<String> uniqueTexts = messages.stream()
+                .map(MessageHelper::getActualText)
+                .distinct()
+                .collect(Collectors.toList());
+        return uniqueTexts.size() == 1 && uniqueTexts.get(0) != null;
     }
 
-    private boolean equalAuthor(Message newMessage){
-        return Objects.equals(lastMessage.getFrom().getId(), newMessage.getFrom().getId());
+    private boolean allStickersEquals(List<Message> messages) {
+        List<String> uniqueStickers = messages.stream()
+            .map(Message::getSticker)
+            .map(sticker -> sticker == null ? null : sticker.getFileId())
+            .distinct()
+            .collect(Collectors.toList());
+        return uniqueStickers.size() == 1 && uniqueStickers.get(0) != null;
+    }
+
+    private int generateRequiredLength() {
+        double random = Math.random();
+        if (random < 0.3) return 2;
+        if (random < 0.8) return 3;
+        else return 4;
     }
 }
