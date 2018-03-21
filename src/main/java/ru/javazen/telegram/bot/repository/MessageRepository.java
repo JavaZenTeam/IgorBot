283cef568b13c857f9a2d4f9231f1f651bd61e3d
Dmitriy <Daemon712@yandex.ru>
2018-03-21 21:21:54 +0400

    fix performance and constraint for MessageCollector

--- src/main/java/ru/javazen/telegram/bot/repository/MessageRepository.java
+++ src/main/java/ru/javazen/telegram/bot/repository/MessageRepository.java
@@ -1,8 +1,13 @@
 package ru.javazen.telegram.bot.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
+import org.springframework.scheduling.annotation.Async;
 import ru.javazen.telegram.bot.model.MessageEntity;
 import ru.javazen.telegram.bot.model.MessagePK;
 
-public interface MessageRepository extends CrudRepository<MessageEntity, MessagePK> {
+import java.util.concurrent.CompletableFuture;
+
+public interface MessageRepository extends Repository<MessageEntity, MessagePK> {
+    @Async
+    <S extends MessageEntity> CompletableFuture<S> save(S message);
 }
