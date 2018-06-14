7285efbbe58d4901fa721653514695d46410287c
Andrew <asmal95@mail.ru>
2018-06-15 00:28:06 +0400

    Added tests for scheduler

---
+++ src/test/java/ru/javazen/telegram/bot/scheduler/SchedulerNotifyHandlerTest.java
@@ -0,0 +1,153 @@
+package ru.javazen.telegram.bot.scheduler;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.telegram.telegrambots.api.objects.Chat;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.User;
+import org.telegram.telegrambots.bots.AbsSender;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import ru.javazen.telegram.bot.scheduler.parser.ScheduledMessageParser;
+import ru.javazen.telegram.bot.scheduler.service.MessageSchedulerService;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SchedulerNotifyHandlerTest {
+
+    private static final int DAYS_LIMIT = 100;
+    private SchedulerNotifyHandler handler;
+
+    @Mock
+    private MessageSchedulerService messageSchedulerService;
+
+    @Mock
+    private ScheduledMessageParser parser;
+
+    @Mock
+    private AbsSender sender;
+
+    private Update update;
+
+    @Before
+    public void init() {
+        handler = new SchedulerNotifyHandler(
+                messageSchedulerService,
+                DAYS_LIMIT,
+                () -> "ok",
+                Collections.singletonList(parser));
+
+        update = mock(Update.class);
+        Message message = mock(Message.class);
+        when(message.getReplyToMessage()).thenReturn(null);
+        when(update.getMessage()).thenReturn(message);
+
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(111);
+        when(message.getFrom()).thenReturn(user);
+
+        Chat chat = mock(Chat.class);
+        when(chat.getId()).thenReturn(222L);
+        when(message.getChat()).thenReturn(chat);
+
+    }
+
+    @Test
+    public void testNullMessage() throws TelegramApiException {
+        when(update.getMessage().getText()).thenReturn(null);
+        Assert.assertFalse(handler.handle(update, sender));;
+    }
+
+    @Test
+    public void testCorrectMessage() throws TelegramApiException {
+        String correctMessage = "Correct Message";
+        when(update.getMessage().getText()).thenReturn(correctMessage);
+
+        when(parser.canParse(correctMessage)).thenReturn(true);
+        ScheduledMessageParser.ParseResult result = new ScheduledMessageParser.ParseResult(new Date(), "test");
+        when(parser.parse(correctMessage, update)).thenReturn(result);
+
+        Assert.assertTrue(handler.handle(update, sender));
+        verify(messageSchedulerService, Mockito.times(1)).scheduleTask(Mockito.any());
+    }
+
+    @Test
+    public void testTooLongDate() throws TelegramApiException {
+        String correctMessage = "Correct Message";
+        when(update.getMessage().getText()).thenReturn(correctMessage);
+
+        when(parser.canParse(correctMessage)).thenReturn(true);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, DAYS_LIMIT + 1);
+
+        ScheduledMessageParser.ParseResult result =
+                new ScheduledMessageParser.ParseResult(calendar.getTime(), "test");
+        when(parser.parse(correctMessage, update)).thenReturn(result);
+
+        Assert.assertTrue(handler.handle(update, sender));
+        verify(messageSchedulerService, Mockito.times(0)).scheduleTask(Mockito.any());
+    }
+
+    @Test
+    public void testWrongMessage() throws TelegramApiException {
+        String correctMessage = "Correct Message";
+        when(update.getMessage().getText()).thenReturn("Wrong Message");
+
+        when(parser.canParse(Mockito.anyString())).thenReturn(false);
+        when(parser.canParse(correctMessage)).thenReturn(true);
+
+        ScheduledMessageParser.ParseResult result = new ScheduledMessageParser.ParseResult(new Date(), "test");
+        when(parser.parse(correctMessage, update)).thenReturn(result);
+
+        Assert.assertFalse(handler.handle(update, sender));
+        verify(messageSchedulerService, Mockito.times(0)).scheduleTask(Mockito.any());
+    }
+
+    @Test
+    public void testWrongParsing() throws TelegramApiException {
+        String message = "Message";
+        when(update.getMessage().getText()).thenReturn(message);
+        when(parser.canParse(message)).thenReturn(true);
+
+        ScheduledMessageParser.ParseResult result = new ScheduledMessageParser.ParseResult(new Date(), "test");
+        when(parser.parse("Wait Other", update)).thenReturn(result);
+
+        Assert.assertFalse(handler.handle(update, sender));
+        verify(messageSchedulerService, Mockito.times(0)).scheduleTask(Mockito.any());
+    }
+
+    @Test
+    public void checkFilteringWithCanParse() throws TelegramApiException {
+        String message = "Message";
+        when(update.getMessage().getText()).thenReturn(message);
+        when(parser.canParse(message)).thenReturn(true);
+
+        handler.handle(update, sender);
+
+        Mockito.verify(parser, Mockito.times(1)).parse(Mockito.anyString(), Mockito.anyObject());
+    }
+
+    @Test
+    public void checkFilteringWithCanNotParse() throws TelegramApiException {
+        String message = "Message";
+        when(update.getMessage().getText()).thenReturn(message);
+        when(parser.canParse(message)).thenReturn(false);
+
+        handler.handle(update, sender);
+
+        Mockito.verify(parser, Mockito.times(0)).parse(Mockito.anyString(), Mockito.anyObject());
+    }
+}
---
+++ src/test/java/ru/javazen/telegram/bot/scheduler/parser/ShiftTimeParserTest.java
@@ -0,0 +1,63 @@
+package ru.javazen.telegram.bot.scheduler.parser;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+
+import java.util.Date;
+
+public class ShiftTimeParserTest {
+
+    private static ShiftTimeParser parser;
+    private static Update update;
+
+
+    @BeforeClass
+    public static void initClass() {
+
+        parser = new ShiftTimeParser(() -> "ok", "и+го+рь,\\s?ск[ао]ж[иы] через( .+)");
+
+        update = Mockito.mock(Update.class);
+        Message message = Mockito.mock(Message.class);
+        Mockito.when(message.getReplyToMessage()).thenReturn(null);
+        Mockito.when(update.getMessage()).thenReturn(message);
+    }
+
+    @Test
+    public void canParse() {
+        Assert.assertTrue(parser.canParse("Игорь, скажи через 2 часа привет"));
+    }
+
+    @Test
+    public void cantParse() {
+        Assert.assertFalse(parser.canParse("Игорь, скажи a"));
+    }
+
+    @Test
+    public void checkDefaultMessage() {
+        ScheduledMessageParser.ParseResult result = parser.parse("Игорь, скажи через 2 часа", update);
+
+        Assert.assertEquals("ok", result.getMessage());
+    }
+
+    @Test
+    public void checkUserMessage() {
+        ScheduledMessageParser.ParseResult result = parser.parse("Игорь, скажи через 2 часа привет", update);
+
+        Assert.assertEquals("привет", result.getMessage());
+    }
+
+    @Test
+    public void checkTime() {
+        ScheduledMessageParser.ParseResult result = parser.parse("Игорь, скажи через 2 часа привет", update);
+        Date now = new Date();
+        int twoHours = 1000 * 60 * 120;
+
+        long diff = Math.abs((now.getTime() + twoHours) - result.getDate().getTime());
+
+        Assert.assertTrue(diff < 1000);
+    }
+}
