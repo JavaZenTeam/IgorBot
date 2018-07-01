fc0d74fee32b3c5ca8397282841f1ff0ae2fa6a2
Andrew <asmal95@mail.ru>
2018-07-01 17:37:17 +0400

    Spring Security for chat statistic protection

--- src/main/java/ru/javazen/telegram/bot/handler/stats/LinkToChatPageHandler.java
+++ src/main/java/ru/javazen/telegram/bot/handler/stats/LinkToChatPageHandler.java
@@ -1,9 +1,11 @@
 package ru.javazen.telegram.bot.handler.stats;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import ru.javazen.telegram.bot.handler.UpdateHandler;
+import ru.javazen.telegram.bot.security.authentication.service.AuthenticationTokenService;
 import ru.javazen.telegram.bot.util.MessageHelper;
 
 import java.text.MessageFormat;
@@ -11,13 +13,18 @@ import java.text.MessageFormat;
 public class LinkToChatPageHandler implements UpdateHandler {
     private String linkTemplate;
 
+    @Autowired
+    private AuthenticationTokenService authenticationTokenService;
+
     public LinkToChatPageHandler(String linkTemplate) {
         this.linkTemplate = linkTemplate;
     }
 
     @Override
     public boolean handle(Update update, AbsSender sender) throws TelegramApiException {
-        String link = MessageFormat.format(linkTemplate, update.getMessage().getChatId());
+        String token = authenticationTokenService.generateToken(update.getMessage().getChatId()).getToken();
+
+        String link = MessageFormat.format(linkTemplate, token);
         sender.execute(MessageHelper.answer(update.getMessage(), link));
         return true;
     }
