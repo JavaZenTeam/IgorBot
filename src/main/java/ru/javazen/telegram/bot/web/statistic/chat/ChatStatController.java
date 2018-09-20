32affdd8cde913487219a607521d57ee0cf3a571
Dmitriy <Daemon712@yandex.ru>
2018-09-21 00:45:16 +0400

    impl end point for top active chat users

--- src/main/java/ru/javazen/telegram/bot/web/statistic/chat/ChatStatController.java
+++ src/main/java/ru/javazen/telegram/bot/web/statistic/chat/ChatStatController.java
@@ -1,29 +1,36 @@
 package ru.javazen.telegram.bot.web.statistic.chat;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.api.methods.groupadministration.GetChat;
 import org.telegram.telegrambots.api.objects.Chat;
 import org.telegram.telegrambots.api.objects.User;
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import ru.javazen.telegram.bot.datasource.ChatDataSource;
+import ru.javazen.telegram.bot.datasource.model.UserStatistic;
 import ru.javazen.telegram.bot.security.authentication.AuthenticationToken;
 import ru.javazen.telegram.bot.security.authentication.service.AuthenticationTokenService;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class ChatStatController {
     private DefaultAbsSender bot;
     private AuthenticationTokenService authenticationTokenService;
+    private ChatDataSource chatDataSource;
 
     @Autowired
-    public ChatStatController(DefaultAbsSender bot, AuthenticationTokenService authenticationTokenService) {
+    public ChatStatController(DefaultAbsSender bot, AuthenticationTokenService authenticationTokenService, ChatDataSource chatDataSource) {
         this.bot = bot;
         this.authenticationTokenService = authenticationTokenService;
+        this.chatDataSource = chatDataSource;
     }
 
     @PreAuthorize("hasAuthority(#chatIdStr)")
@@ -33,6 +40,34 @@ public class ChatStatController {
         Chat chat = bot.execute(new GetChat(chatId));
         model.addAttribute("chat", chat);
         return "chat";
+    }
+
+    @PreAuthorize("hasAuthority(#chatIdStr)")
+    @GetMapping("/chat/{chatId}/topActiveUsers")
+    @ResponseBody
+    public List<UserStatistic> topActiveUsers(
+            @PathVariable("chatId")
+                    String chatIdStr,
+            @RequestParam(value = "fromDate", required = false)
+            @DateTimeFormat(pattern = "dd.MM.yyyy")
+                    Date fromDate,
+            @RequestParam(value = "toDate", required = false)
+            @DateTimeFormat(pattern = "dd.MM.yyyy")
+                    Date toDate) {
+
+        Long chatId = Long.valueOf(chatIdStr);
+
+        if (toDate == null) {
+            toDate = new Date();
+        }
+        if (fromDate == null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(toDate);
+            calendar.add(Calendar.MONTH, -1);
+            fromDate = calendar.getTime();
+        }
+
+        return chatDataSource.topActiveUsers(chatId, fromDate, toDate);
     }
 
     @GetMapping("/stats/{token}")
