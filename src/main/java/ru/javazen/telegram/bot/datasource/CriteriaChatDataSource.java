baec857feb79345b914a2d8c8f95508b188def9e
Dmitry <Daemon712@yandex.ru>
2018-11-05 22:00:05 +0400

    October fixes (#13)
    * make sayTextHandler optional and depends on profile 'say-text'
    
    * remove default value for IGOR_BOT_SUPPORT_CHAT because app's start was failing if bot has not access to that chat, IGOR_BOT_SUPPORT_CHAT is optional now
    
    * add default value for BASE_URL. It's valid for local environment, but you need override it for remote one
    
    * implement a Filter to match correctly messages in format /command@botname
    
    * improve design of chat stats
    
    * Fix warning message for empty word usage statistic
    
    * Fix query for Messages count

--- src/main/java/ru/javazen/telegram/bot/datasource/CriteriaChatDataSource.java
+++ src/main/java/ru/javazen/telegram/bot/datasource/CriteriaChatDataSource.java
@@ -75,4 +75,18 @@ public class CriteriaChatDataSource implements ChatDataSource {
                 .setMaxResults(50)
                 .getResultList();
     }
+
+    @Override
+    public Long messagesCount(Long chatId, Date after, Date before) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> query = builder.createQuery(Long.class);
+        Root<MessageEntity> messages = query.from(MessageEntity.class);
+        query.where(
+                builder.equal(messages.get(MessageEntity_.chat), chatId),
+                builder.between(messages.get(MessageEntity_.date), after, before));
+        query.select(builder.count(messages));
+        List<Long> resultList = entityManager.createQuery(query)
+                .getResultList();
+        return resultList.isEmpty() ? 0L : resultList.get(0);
+    }
 }
