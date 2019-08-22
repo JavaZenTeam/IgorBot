8be306a787f67b88431a77d633b289a0d8ed1080
skroy <mareev.yegor@gmail.com>
2019-08-23 00:28:35 +0400

    Fix tests

--- src/main/resources/db/migration/common/V3.5__Scheduler_Repetition.sql
+++ src/main/resources/db/migration/common/V3.5__Scheduler_Repetition.sql
@@ -1,3 +1,4 @@
 ALTER TABLE message_task
-ADD COLUMN repeat_interval varchar(255) null;
-ADD COLUMN repeat_count integer null;
+    ADD COLUMN repeat_interval varchar(255) null;
+ALTER TABLE message_task
+    ADD COLUMN repeat_count integer null;
