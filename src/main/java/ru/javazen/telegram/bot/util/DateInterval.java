ece328d5dc6c13f3d751b843672fd0eb3fe48350
skroy <skroychan@gmail.com>
2019-09-02 15:44:32 +0400

    Fix repetition limit check

--- src/main/java/ru/javazen/telegram/bot/util/DateInterval.java
+++ src/main/java/ru/javazen/telegram/bot/util/DateInterval.java
@@ -19,10 +19,21 @@ public class DateInterval {
         Calendar.MINUTE,
         Calendar.SECOND
     };
-    
+
+    private final static String[][] TEXT_TIME_UNITS_RUS = {
+            { "год", "года", "лет" },
+            { "месяц", "месяца", "месяцев" },
+            { "неделю", "недели", "недель" },
+            { "день", "дня", "дней" },
+            { "час", "часа", "часов" },
+            { "минуту", "минуты", "минут" },
+            { "секунду", "секунды", "секунд" }
+    };
+
     public static Date apply(String interval, Date date) {
 
         GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
         Pattern pattern = Pattern.compile(INTERVAL_REGEXP);
         Matcher matcher = pattern.matcher(interval);
 
@@ -35,5 +46,25 @@ public class DateInterval {
         }
 
         return calendar.getTime();
+    }
+
+
+    public static String getText(String interval) {
+
+        Pattern pattern = Pattern.compile(INTERVAL_REGEXP);
+        Matcher matcher = pattern.matcher(interval);
+        StringBuilder sb = new StringBuilder();
+
+        matcher.matches();
+        for (int i = 0; i < TIME_UNITS.length; i++) {
+            String match = matcher.group(i + 1);
+            if (!match.isEmpty()) {
+                int value = Integer.parseInt(match);
+                sb.append(match + " ");
+                sb.append(TEXT_TIME_UNITS_RUS[i][value == 1 ? 0 : value > 1 && value < 5 ? 1 : 2] + " ");
+            }
+        }
+
+        return sb.toString();
     }
 }
