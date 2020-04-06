a07ad53caf923e566eb08ba264727ef5e20417bb
asmal95 <asmal95@mail.ru>
2020-04-07 01:22:46 +0400

    Add datetime tag

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -7,7 +7,13 @@ docker build -t $IMAGE_NAME .
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
     docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
-    FULL_IMAGE_NAME="${DOCKER_IMAGE_NAME}:latest"
-    docker tag $IMAGE_NAME $FULL_IMAGE_NAME
-    docker push $FULL_IMAGE_NAME
+    FULL_IMAGE_NAME_LATEST="${DOCKER_IMAGE_NAME}:latest"
+    DATE_TAG=$(date '+%d%m%Y_%H%M%S')
+    FULL_IMAGE_NAME_TAG="${DOCKER_IMAGE_NAME}:${DATE_TAG}"
+
+    docker tag $IMAGE_NAME $FULL_IMAGE_NAME_LATEST
+    docker tag $IMAGE_NAME $FULL_IMAGE_NAME_TAG
+
+    docker push $FULL_IMAGE_NAME_LATEST
+    docker push $FULL_IMAGE_NAME_TAG
 fi
