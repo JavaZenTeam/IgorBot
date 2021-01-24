590e2b08a198396b48b40e4ecfe1e13834817a43
Dmitriy <Daemon712@yandex.ru>
2021-01-24 13:08:23 +0400

    Chat Events

--- src/main/java/ru/javazen/telegram/bot/model/UserEntity.java
+++ src/main/java/ru/javazen/telegram/bot/model/UserEntity.java
@@ -1,16 +1,15 @@
 package ru.javazen.telegram.bot.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import java.util.Objects;
 
 @Entity
-@Table
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity {
@@ -25,59 +24,4 @@ public class UserEntity {
 
     @Column(length = 32)
     private String username;
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "userId=" + userId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", username='" + username + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserEntity that = (UserEntity) o;
-        return Objects.equals(getUserId(), that.getUserId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getUserId());
-    }
 }
