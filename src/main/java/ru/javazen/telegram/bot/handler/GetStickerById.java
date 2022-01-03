3d9636f292640ddfe905e658ebeb12a73dab64da
Dmitry <Daemon712@yandex.ru>
2022-01-03 16:42:25 +0400

    improve logging errors (#90)

--- src/main/java/ru/javazen/telegram/bot/handler/GetStickerById.java
+++ src/main/java/ru/javazen/telegram/bot/handler/GetStickerById.java
@@ -1,18 +1,22 @@
 package ru.javazen.telegram.bot.handler;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import ru.javazen.telegram.bot.handler.base.TextMessageHandler;
 
+import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GetStickerById implements TextMessageHandler {
     private static final Pattern DEFAULT_PATTERN = Pattern.compile("/sticker (.+)");
     private Pattern pattern = DEFAULT_PATTERN;
+    private Supplier<String> failedResponseSupplier;
 
     @Override
     public boolean handle(Message message, String text, AbsSender sender) throws TelegramApiException {
@@ -23,12 +27,27 @@ public class GetStickerById implements TextMessageHandler {
         sendSticker.setChatId(message.getChatId().toString());
         sendSticker.setSticker(new InputFile(matcher.group(1)));
 
-        sender.execute(sendSticker);
+        try {
+            sender.execute(sendSticker);
+        } catch (TelegramApiRequestException e) {
+            if (e.getApiResponse().contains("wrong remote file identifier specified")) {
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(message.getChatId().toString());
+                sendMessage.setText(failedResponseSupplier.get());
+                sender.execute(sendMessage);
+            } else {
+                throw e;
+            }
+        }
 
         return true;
     }
 
     public void setPattern(String pattern) {
         this.pattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+    }
+
+    public void setFailedResponseSupplier(Supplier<String> failedResponseSupplier) {
+        this.failedResponseSupplier = failedResponseSupplier;
     }
 }
